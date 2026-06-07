da2aaf9ae74eca584127fe7433e8b9e9cceb2d24
Gigi <dergigi@pm.me>
2026-06-07 23:38:32 +0200

    fix: hide Submit Design button on judging page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -88,7 +88,6 @@ export function Header({
               <Link to="/" className={navLink}>
                 Gallery
               </Link>
-              <SubmitDesignButton />
               <LoginButton />
             </>
           )}
