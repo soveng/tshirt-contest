f6af1ec899c8ac96c4e2ae9320fc9a0124aae247
Gigi <dergigi@pm.me>
2026-06-08 00:00:08 +0200

    fix: use tshirt.sovereignengineering.io as default site URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-const DEFAULT_SITE_URL = "https://tshirt-contest.vercel.app";
+const DEFAULT_SITE_URL = "https://tshirt.sovereignengineering.io";
 
 function siteMetaPlugin(siteUrl: string) {
   const normalized = siteUrl.replace(/\/$/, "");
